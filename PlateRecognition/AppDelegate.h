a70266f7de87392264dffb9a85e75d50ee6e3f0f
AndyLi2020 <Xiaomei@encompass8.cn>
2016-09-01 14:31:11 +0800

    Initial Commit

---
+++ PlateRecognition/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PlateRecognition
+//
+//  Created by Xiaomei LI on 9/1/16.
+//  Copyright © 2016 Xiaomei LI. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
